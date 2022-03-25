4a6a255ead16106417297bea0155317eb79678f8
chris konstantinou <chriskon97@gmail.com>
2022-03-25 13:24:28 +0200

    Create Solution.cpp

---
+++ 1029. Two City Scheduling/Solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        return 1;
+    }
+};
+
+int main ()
+{
+    Solution solution;
+    cout << solution.twoCitySchedCost({{259, 770}, 
+                                       {448, 54}, 
+                                       {926, 667}, 
+                                       {184, 139}, 
+                                       {840, 118}, 
+                                       {577, 469}}) << endl;
+    return 0;
+}
